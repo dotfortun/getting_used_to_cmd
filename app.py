750be63dd63e1b8726d9c41aba2f0e53b00813a3
Shane Bell <being.for.itself@gmail.com>
2022-01-14 16:01:49 -0500

    Breaking stuff if the config doesn't exist.

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__, static_url_path="/static", static_folder="static")
 
 @app.route('/')
 def hello_world():
-    if os.path.isdir("./static/empty_folder"):
+    if os.path.isdir("./static/empty_folder") and os.path.isfile("./config.file"):
         return render_template("base.html")
     else:
+        raise Exception("Missing empty_folder or config.file.")
         abort(404)
 
 
