3244db2f466ca35ce6897893edcd4e20a21ae7b3
Shane Bell <being.for.itself@gmail.com>
2022-01-14 15:49:23 -0500

    Adding missing directory

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+import os.path
+
+from flask import Flask, render_template, abort
 
 app = Flask(__name__, static_url_path="/static", static_folder="static")
 app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -6,7 +8,10 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 def hello_world():
-    return render_template("base.html")
+    if os.path.isdir("./static/empty_folder"):
+        return render_template("base.html")
+    else:
+        abort(404)
 
 
 if __name__ == '__main__':
